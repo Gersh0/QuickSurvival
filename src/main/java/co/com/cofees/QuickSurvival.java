79f8eeeb3ce044314f7fb78274e4b3f168c9efa7
BuildTools <unconfigured@null.spigotmc.org>
2024-01-26 20:54:59 -0500

    Agregado OnEnable y OnDisable

--- src/main/java/co/com/cofees/QuickSurvival.java
+++ src/main/java/co/com/cofees/QuickSurvival.java
@@ -1,6 +1,17 @@
 package co.com.cofees;
 
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class QuickSurvival extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        Bukkit.getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&',"&bHola"));//Versión, Prefix PluginName
+    }
+
+    @Override
+    public void onDisable() {
+        Bukkit.getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&',"&bChao"));
+    }
 }
