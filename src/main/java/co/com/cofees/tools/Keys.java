0390272a57fbbd764a06820d2ebd4723536b3116
Vrtx566 <jose.gomez4@eia.edu.co>
2024-02-11 16:26:31 -0500

    Eventos agregados, menu de waystone funcional y nuevo comando para abrir manualmente el waystoneMenu tambien

--- src/main/java/co/com/cofees/tools/Keys.java
+++ src/main/java/co/com/cofees/tools/Keys.java
@@ -1,8 +1,8 @@
 package co.com.cofees.tools;
-
 import co.com.cofees.QuickSurvival;
 import org.bukkit.NamespacedKey;
-
 public class Keys {
-    public static final NamespacedKey WAYSTONE= new NamespacedKey(QuickSurvival.getInstance(), "Waystone");
+    public static final NamespacedKey WAYSTONE = new NamespacedKey(QuickSurvival.getInstance(),"Waystone");
+
+
 }
