9924ce444bbebe70637ce00ff591e8c1c3e41147
vrtx <jose.gomez4@eia.edu.co>
2024-02-18 21:36:22 -0500

    Changed QuickSurvival.java

--- src/main/java/co/com/cofees/QuickSurvival.java
+++ src/main/java/co/com/cofees/QuickSurvival.java
@@ -2,9 +2,12 @@ package co.com.cofees;
 
 import co.com.cofees.commands.TestCommand;
 import co.com.cofees.events.TestEvent;
+import co.com.cofees.recipes.CustomRecipes;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
+import co.com.cofees.events.*;
+import co.com.cofees.commands.*;
 
 public class QuickSurvival extends JavaPlugin {
 
@@ -15,6 +18,8 @@ public class QuickSurvival extends JavaPlugin {
         Bukkit.getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', "&bPlugin enabled."));//Versión, Prefix PluginName
         registerCommand();
         registerEvents();
+        CustomRecipes.registerCustomCrafting();
+
         this.prueba = true;
     }
 
@@ -24,10 +29,25 @@ public class QuickSurvival extends JavaPlugin {
     }
 
     public void registerCommand() {
-        this.getCommand("test").setExecutor(new TestCommand());
+        this.getCommand("test").setExecutor(new NewTestCommand(this));
+        this.getCommand("inventory").setExecutor(new WaystoneCommand());
+        this.getCommand("waystone").setExecutor(new WaystoneBannerInteract());
+        this.getCommand("backpack").setExecutor(new BackpackCommand());
+        this.getCommand("qspanel").setExecutor(new ControlPanelCommmand());
     }
 
     public void registerEvents() {
         getServer().getPluginManager().registerEvents(new TestEvent(), this);
+        getServer().getPluginManager().registerEvents(new ControlPanelListener(),this);
+        getServer().getPluginManager().registerEvents(new WaystoneMenu(),this);
+        getServer().getPluginManager().registerEvents(new BackpackInteract(), this);
+        getServer().getPluginManager().registerEvents(new WaystonePlacement(),this);
+        getServer().getPluginManager().registerEvents(new WaystoneInteract(),this);
+
+
+    }
+
+    public static QuickSurvival getInstance(){
+        return getPlugin(QuickSurvival.class);
     }
 }
