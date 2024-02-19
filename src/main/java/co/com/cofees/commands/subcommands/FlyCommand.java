40c9853b31a6890189b7e399befdaa56613ae611
Gersh0 <germinatorcofee@gmail.com>
2024-02-19 07:20:52 -0500

    Commands commented :D

--- src/main/java/co/com/cofees/commands/subcommands/FlyCommand.java
+++ src/main/java/co/com/cofees/commands/subcommands/FlyCommand.java
@@ -21,43 +21,62 @@ public class FlyCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, String[] args) {
-
+        // If the sender is not a player and no arguments are provided
         if ((!(sender instanceof Player)) && args.length == 0) {
+            // Log a warning message indicating that this command can't be used from the console
             sender.getServer().getLogger().warning("Can't use this command from console.");
             return true;
         }
 
+        // If the sender is not a player
         if (!(sender instanceof Player)) {
+            // Log an info message indicating that fly mode has been enabled for the player specified in the arguments
             sender.getServer().getLogger().info("Fly mode enabled for " + args[0]);
+            // Toggle fly mode for the player specified in the arguments
             return toggleFly(Objects.requireNonNull(sender.getServer().getPlayer(args[0])));
         }
 
+        // Cast the sender to a Player
         Player player = (Player) sender;
+        // If no arguments are provided
         if (args.length == 0) {
+            // Toggle fly mode for the player who sent the command
             return toggleFly(player);
         }
 
+        // If none of the online players' names match the name provided in the arguments
         if (sender.getServer().getOnlinePlayers().stream()
                 .map(Player::getName)
                 .noneMatch(name -> name.equalsIgnoreCase(args[0]))) {
+            // Send a message to the sender
             sender.sendMessage("Hi");
 
             return false;
         }
+        // Send a message to the sender indicating that fly mode has been toggled for the player specified in the arguments
         sender.sendMessage("Fly mode toggled for " + args[0]);
+        // Toggle fly mode for the player specified in the arguments
         toggleFly(Objects.requireNonNull(sender.getServer().getPlayer(args[0])));
 
         return false;
     }
 
     public boolean toggleFly(Player player) {
+        // If the player does not currently have flight enabled
         if (!player.getAllowFlight()) {
+            // Send a message to the player indicating that fly mode has been enabled
             player.sendMessage(text("&bFly mode enabled for " + player.getName()));
+            // Enable flight for the player
             player.setAllowFlight(true);
+            // Return true indicating that the operation was successful
             return true;
         }
+        // Send a message to the player indicating that fly mode has been disabled
         player.sendMessage(text("&bFly mode disabled for " + player.getName()));
+        // Disable flight for the player
         player.setAllowFlight(false);
+        // Return true indicating that the operation was successful
         return true;
     }
+
 }
