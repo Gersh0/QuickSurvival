67833986d836b82b0a91c2f8699efd18e1c9b94c
vrtx <jose.gomez4@eia.edu.co>
2024-02-23 07:23:42 -0500

    agregado version chafa de guardado

---
+++ src/main/java/co/com/cofees/events/BackpackSaver.java
@@ -0,0 +1,36 @@
+package co.com.cofees.events;
+
+import co.com.cofees.QuickSurvival;
+import co.com.cofees.tools.Keys;
+import org.bukkit.ChatColor;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryCloseEvent;
+import org.bukkit.event.inventory.InventoryOpenEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.persistence.PersistentDataContainer;
+import org.bukkit.persistence.PersistentDataType;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.Objects;
+
+public class BackpackSaver implements Listener {
+
+
+
+
+
+
+
+
+
+
+
+}
