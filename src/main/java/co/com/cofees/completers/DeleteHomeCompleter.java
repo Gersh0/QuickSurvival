faca523dfdcd7b2f7988425093ea515fa17d2e1a
BuildTools <unconfigured@null.spigotmc.org>
2024-02-25 16:01:58 -0500

    Added home functionality

---
+++ src/main/java/co/com/cofees/completers/DeleteHomeCompleter.java
@@ -0,0 +1,17 @@
+package co.com.cofees.completers;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
+
+public class DeleteHomeCompleter implements TabCompleter {
+    @Nullable
+    @Override
+    public List<String> onTabComplete(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] strings) {
+        return null;
+    }
+}
